f08a302bc5f4603de15cc69bfdd0f5f4ba0bbc55
mickey-40 <mickeyarnold53@yahoo.com>
2022-11-28 15:04:37 -0600

    styled the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
-    <footer id="footer">
-      <div>
+    <footer id='footer'>
+      
           <span>Burgers and Beers </span>
             <span>&copy; 2022 burgersandbeers.com</span>
-      </div>
+ 
       
     </footer>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ export default function Home(props) {
   return (
     <>
       <main>
-        <h2>Welcome to the homepage!</h2>
-        <h1>An App for People who love eating and drinking good food.</h1>
+        <h2>Welcome to the Burgers and Beers!</h2>
+        {props.user ?  null:<h1>An App for People who love eating and drinking good food.</h1>}
         <h3>{props.user ? 'Welcome back ' + props.user.username.charAt(0).toUpperCase()+ props.user.username.slice(1)+'!': 'Just Browse Around or Register/Login to join the fun.'}</h3>
         <img src="https://images.pexels.com/photos/1267696/pexels-photo-1267696.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="People having fun" height={300} width={500}></img>
       </main>
